37be1971ae41f75cfc7b3989177732a39d19a2a5
vejryn <envyero@gmail.com>
2020-10-07 19:56:33 +0700

    Add setting and main files.

---
+++ main.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import math
+import os.path
+import time
+from bitmex import bitmex
+from datetime import timedelta, datetime
+from dateutil import parser
+from tqdm import tqdm #(Optional, used for progress-bars)
+from setting import API_KEY, API_SECRET
+
+bitmex_client = bitmex(test=False, api_key=API_KEY, api_secret=API_SECRET)
+
+symbol = 'XBT'
+count = 500
+
+def minutes_of_new_data(symbol, data):
+    if len(data) < 0:
+        old = parser.parse(data['timestamp'].iloc[-1])
+    elif symbol == 'XBT':
+        old = bitmex_client.Funding.Funding_get(symbol=symbol, reverse=False, count=count).result()[0][0]['timestamp']
+    if symbol == 'XBT':
+        new = bitmex_client.Funding.Funding_get(symbol=symbol, reverse=True, count=count).result()[0][0]['timestamp']
+    return old, new
+
+def get_funding_rate(symbol):
+    filename = '%s-funding-rate.csv' % (symbol)
+    data_df = pd.DataFrame()
+    oldest_point, newest_point = minutes_of_new_data('XBT', data_df)
+    delta_min = (newest_point - oldest_point).total_seconds()/60
+    available_data = math.ceil(delta_min/480)
+    rounds = math.ceil(available_data / count)
+    if rounds > 0:
+        print('Downloading funding rate')
+        for round_num in tqdm(range(rounds)):
+            time.sleep(1)
+            new_time = (oldest_point + timedelta(minutes = round_num * count * 480))
+            data = bitmex_client.Funding.Funding_get(symbol=symbol, reverse=True, count=count, startTime = new_time).result()[0]
+            temp_df = pd.DataFrame(data)
+            data_df = data_df.append(temp_df)
+    data_df.set_index('timestamp', inplace=True)
+    data_df.to_csv(filename)
+    return data_df
+
+get_funding_rate('XBT')
---
+++ setting.py
@@ -0,0 +1,6 @@
+#########
+## Replace API_KEY and API_SECRET from your own
+## DO NOT SHARE API_KEY AND API_SECRET TO ANYONE
+
+API_KEY = '#'
+API_SECRET = '#'
